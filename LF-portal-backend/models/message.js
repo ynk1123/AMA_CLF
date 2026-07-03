561285b5477f055bedd7c35467dfb71ef706556e
Yuna Mendoza <yunajliannemndza@gmail.com>
2026-07-03 16:44:57 +0800

    Fix message model import casing

--- LF-portal-backend/models/message.js
+++ LF-portal-backend/models/message.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require('sequelize');
 const { sequelize } = require('../config/database');
-const User = require('./User');
+const User = require('./user');
 
 const Message = sequelize.define('Message', {
   id: {
