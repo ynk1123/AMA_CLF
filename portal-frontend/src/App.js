1c4056a276274ed98f97d85d7a003af5a92b2790
Yuna Mendoza <yunajliannemndza@gmail.com>
2026-07-05 12:41:28 +0800

    Use HashRouter for email reset links

--- portal-frontend/src/App.js
+++ portal-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider, CssBaseline, Box } from '@mui/material';
 import { AuthProvider } from './context/AuthContext';
 import { LanguageProvider } from './context/LanguageContext';
