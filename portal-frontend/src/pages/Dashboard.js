082acf38c1c6458f1beb65e2758cffba473f75b6
ynk11_23 <158818269+ynk1123@users.noreply.github.com>
2026-07-14 18:07:41 +0800

    Update Dashboard.js inalis ko yun )} sa 1162

--- portal-frontend/src/pages/Dashboard.js
+++ portal-frontend/src/pages/Dashboard.js
@@ -1159,7 +1159,7 @@ return (
               alt={selectedItem.title}
               sx={{ width: '100%', borderRadius: 2 }}
             />
-           )}       
+           
         </Grid>
         <Grid item xs={12} md={6}>     
         )}
